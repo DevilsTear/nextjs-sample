4756fd57ddf450eedaf635a885365dddbbd81ba7
Haluk Alkın Turan <haluk.a.turan@gmail.com>
2022-06-19 08:20:22 +0300

    SSR - getStaticProps added

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,30 @@
-function HomePage(){
+import path from 'path';
+import fs from 'fs/promises';
+
+function HomePage(props){
+    const {products} = props;
+
     return <div>
         <h1>The Home Page</h1>
+        <ul>
+            {products.map((product) => (
+                <li key={product.id}>{product.title}</li>
+            ))}
+        </ul>
     </div>;
+}
+
+export async function getStaticProps(){
+    const dataFilePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+    const jsonData = await fs.readFile(dataFilePath);
+    const data = JSON.parse(jsonData);
+
+    return {
+        props: {
+            products: data.products
+        },
+        revalidate: 10 // second(s) - Incremental Static Generation (ISR)
+    };
 }
 
 export default HomePage;
